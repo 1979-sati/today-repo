514c160161b29c642d1e8b65f1bb731e78fa8f01
1979-sati <kumar.satish3000@gmail.com>
2026-06-02 18:46:14 +0530

    Add a second resource group 'rg1' in rg.tf

--- rg.tf
+++ rg.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "rg" {
   name     = "tinku_rg"
   location = "West Europe"
 }
+
+resource "azurerm_resource_group" "rg1" {
+  name     = "tinku_rg1"
+  location = "West Europe"
+}
